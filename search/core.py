a8ac0d30a63c2b700523454a065954ccfde68ca0
yousef293 <y.galal293@gmail.com>
2026-05-03 19:24:26 +0300

    .

--- search/core.py
+++ search/core.py
@@ -1,20 +1,32 @@
+from engine import get_successor
+from engine.goal import goal_test  
 
+def solve(strategy, initial_state, grid):
+    fringe = []
+    visited = set()
 
-def solve(strategy,intial_state):
-	fringe=[]; visited=[]
-	initial_node=init_node(strategy,intial_state)
-	fringe.append(initial_node)
-	while len(fringe)>0:
-		current_node=fringe.pop(select_node(strategy,fringe))
-		if current_node['state'] in visited : continue
-		visited.append(current_node['state'])
-		if isgoal(current_node['state']):
-			return get_solution(strategy,current_node,len(visited))
-		possible_actions=get_actions(current_node['state'])
-		for action in possible_actions:
-			next_node=add_node(strategy,current_node,action)
-			fringe.append(next_node)
-	return None
+    initial_node = init_node(strategy, initial_state)
+    fringe.append(initial_node)
+
+    while fringe:
+        current_node = fringe.pop(select_node(strategy, fringe))
+        state = current_node['state']
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        if goal_test(state,grid):
+            return get_solution(strategy, current_node, len(visited))
+
+        successors = get_successor(state, grid)
+
+        for action, next_state in successors:
+            next_node = add_node(strategy, current_node, action, next_state)
+            fringe.append(next_node)
+
+    return None
 
 def select_node(strategy,fringe):
 	if strategy == 'DFS': return -1
@@ -27,22 +39,24 @@ def init_node(strategy,intial_state):
 	initial_node = {}
 	initial_node['state']=intial_state
 	initial_node['path']=[]
-	if strategy=='UCS': initial_node['cost']=0
 	return initial_node
 
-def add_node(strategy,current_node,action):
-	next_node = {}
-	next_node['state']=get_state(action,current_node['state'])
-	next_node['path']=current_node['path'][:]; next_node['path'].append(action)
+def add_node(strategy, current_node, action, next_state):
+    next_node = {}
 
-	return next_node
+    next_node['state'] = next_state
+
+    next_node['path'] = current_node['path'][:]
+    next_node['path'].append(action)
+
+
+
+    return next_node
 
 def get_solution(strategy,current_node,time):
 	solution={}
 	solution['solution']=current_node['path']
 	solution['time']=time
-	if strategy=='UCS':
-		solution['cost']=current_node['cost']
 	return solution
 
 
