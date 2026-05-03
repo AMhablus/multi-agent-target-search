5f4dc4a82fc1263b560b3b28cbc01c1986d492d4
abdallahhablus <145384221+abdallahhablus@users.noreply.github.com>
2026-05-03 21:11:19 +0300

    add actions

--- engine/successor.py
+++ engine/successor.py
@@ -3,13 +3,17 @@ from engine.grid import Grid
 from engine.rules import is_valid_transition
 from engine.transitions import apply_joint_action, MOVES
 from engine.state_ops import update_visited_mask
+from typing import Tuple
 
 
-def get_successor(state: State, grid:Grid) -> list[State]:
+Actions = Tuple[str,str]
+
+def get_successor(state: State, grid:Grid) -> list[tuple[State, Actions]]:
     successors = []
-    for m1 in MOVES:
-        for m2 in MOVES:
+    for name1,m1 in MOVES.items():
+        for name2,m2 in MOVES.items():
             joint_move = (m1, m2)
+            action = (name1, name2)
 
             # 1) apply move
             new_positions = apply_joint_action(state.positions, joint_move)
@@ -24,7 +28,7 @@ def get_successor(state: State, grid:Grid) -> list[State]:
             # 4) create new state
             new_state = State(new_positions, new_mask)
 
-            successors.append(new_state)
+            successors.append((new_state, action))
 
     return successors
      
